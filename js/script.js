15b1c998e1146a5919140d42df1c41c2ae5f4321
Patrick Louis Aldover <pl.aldover@gmail.com>
2023-10-25 22:28:42 +0200

    Init

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function toggleSubsections(value) {
+    span = value.getElementsByClassName("section-opener")[0]
+    writingMode = span.style.writingMode
+    span.style.writingMode = (writingMode == 'vertical-lr') ? 'horizontal-tb' : 'vertical-lr';
+}
+
+let sections = document.querySelectorAll('.section');
+
+sections.forEach(section => {
+    section.addEventListener('click', (e) => {
+
+    const menuItemData = e.target.nextElementSibling;
+    menuItemData.style.setProperty('--openHeight', menuItemData.scrollHeight + 'px');
+    menuItemData.classList.toggle('show');
+    menuItemData.classList.toggle('hide');
+
+    const arrow = e.target.getElementsByClassName("section-opener")[0]
+    arrow.classList.toggle('show');
+    arrow.classList.toggle('hide');
+  })
+});
